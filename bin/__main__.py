58f3c88e50e1652b8657eb0464da4330d00c1b19
powellor <oliver.powell@kaust.edu.sa>
2024-02-06 10:01:19 +0300

    commented out assemble-gene code due to bug

--- bin/__main__.py
+++ bin/__main__.py
@@ -3,7 +3,7 @@ import logging
 import sys
 import click
 from blast_2_bed import blast2bed
-from assemble_gene import assemble_gene
+#from assemble_gene import assemble_gene
 
 logging.basicConfig(
     stream=sys.stderr,
@@ -20,7 +20,7 @@ def cli() -> None:
 
 
 cli.add_command(blast2bed)
-cli.add_command(assemble_gene)
+#cli.add_command(assemble_gene)
 
 if __name__ == "__main__":
     cli()
