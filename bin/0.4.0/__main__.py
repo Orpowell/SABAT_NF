72fbb506c1a5bcf18091381770816e4f21e10ad8
powellor <oliver.powell@kaust.edu.sa>
2024-03-28 19:50:19 +0300

    added bin

---
+++ bin/0.4.0/__main__.py
@@ -0,0 +1,27 @@
+import logging
+import sys
+import click
+from blast_2_bed import blast2bed
+from assemble_gene import assemble_exons
+from assemble_gene import assemble_locus
+
+logging.basicConfig(
+    stream=sys.stderr,
+    format="%(asctime)s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+    level=logging.INFO,
+)
+
+
+@click.group(help="S.A.B.A.T: Semi-Automatic BLAST Annotation Toolkit")
+@click.version_option("-v", "--version", message="SABAT 0.4.0")
+def cli() -> None:
+    pass
+
+
+cli.add_command(blast2bed)
+cli.add_command(assemble_exons)
+cli.add_command(assemble_locus)
+
+if __name__ == "__main__":
+    cli()
